4f37b31eb2b787598aa144fca1039e8f647ff425
Michael Berggren <michael.berggren82@gmail.com>
2016-11-26 14:05:01 +0100

    randomsentence

---
+++ Word game/Word game/randomizedSentences.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_game
+{
+    class randomizedSentences
+    {
+        private List<String> randomSentences;
+
+        public List<String> RandomSentences
+        {
+            get { return randomSentences; }
+            set { randomSentences = value; }
+        }
+
+    }
+}
